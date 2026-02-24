ce03b06890b063c00377b1817b97bd4b14972be4
Yuri-m-b <yuri925@hotmail.com>
2026-02-24 20:15:30 -0300

    [python] Adding simple python code

---
+++ python_tools/simple_code/main.py
@@ -0,0 +1,18 @@
+"""This is a simple Python code example for Bazel."""
+
+import logging
+
+def main():
+    """Main function."""
+
+    logging.basicConfig(level=logging.INFO)
+
+    logging.info("Hello, Bazel!")
+    logging.warning("This is a warning message.")
+    logging.error("This is an error message.")
+
+
+
+
+if __name__ == "__main__":
+    main()
